1aedc7a1617459387d69fe2dcdf23da8a505d591
Brian <b.doersch5173@edmail.edcc.edu>
2018-05-04 23:36:41 -0700

    gitignore with testing asp deployment after installation of VS plugin

---
+++ Trackademy/Trackademy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Trackademy.Startup))]
+namespace Trackademy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
